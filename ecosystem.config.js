46ea781bc762fd882164fecb847e480ed94a7d9b
Stefania <stefania.dibacco@gmail.com>
2021-10-28 12:06:30 -0300

    30entregable

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,18 @@ module.exports = {
   apps: [
     {
       name: 'app-fork',
-      script: './dist/src/index.js',
+      script: './dist/index.js',
       watch: true,
       autorestart: true,
-      args: '--mode fork --run pm2',
+      args: '--mode fork --run pm2 --port 8081',
     },
     {
       name: 'app-cluster',
-      script: './dist/src/index.js',
+      script: './dist/index.js',
       watch: true,
       autorestart: true,
-      instances: -1,
-      args: '--mode cluster --run pm2',
+      instances: 4,
+      args: '--mode cluster --run pm2 --port 8082',
     },
   ],
 };
